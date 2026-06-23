62ab11d604f7282d6e6c37f9e4628b95970c928c
Shlomi <shlomikaufman91@gmail.com>
2026-06-23 16:01:38 +0300

    Add exercise 2

--- closures/closure.js
+++ closures/closure.js
@@ -7,3 +7,15 @@ function createLogger() {
 }
 const print = createLogger();
 print();
+
+// Exercise 2
+console.log("\n=== Exercise 2 ===");
+function createGreeting() {
+  let name;
+  return function (name) {
+    console.log(`Hello ${name}`);
+  };
+}
+
+const greeting = createGreeting();
+greeting("Shlomi");
