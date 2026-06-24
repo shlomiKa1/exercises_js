82df4ece5df47f1a4f345b0990d4cb278aa2353a
Shlomi <shlomikaufman91@gmail.com>
2026-06-24 12:24:39 +0300

    Add exercise 5 of commonjs

--- week_01/imports/Part _A/main.js
+++ week_01/imports/Part _A/main.js
@@ -2,6 +2,7 @@ const { add } = require("./math.js");
 const { helloName } = require("./greet.js");
 const { person } = require("./object.js");
 const { adds, substract } = require("./calculate.js");
+const { addition, subtraction, multiplier } = require("./utils.js");
 
 // Exercise 1
 console.log("=== Exercise 1 ===");
@@ -15,7 +16,13 @@ console.log(helloName("Shlomi"));
 console.log("\n=== Exercise 3 ===");
 console.log(person);
 
-// exercise 3
-console.log("\n=== Exercise 3 ===");
+// exercise 4
+console.log("\n=== Exercise 4 ===");
 console.log(`5 + 4 = ${adds(5, 4)}`);
 console.log(`5 - 4 = ${substract(5, 4)}`);
+
+// Exercise 5
+console.log("\n=== Exercise 5 ===");
+console.log(`5 + 5 = ${addition(5, 5)}`);
+console.log(`5 - 5 = ${subtraction(5, 5)}`);
+console.log(`5 * 5 = ${multiplier(5, 5)}`);
---
+++ week_01/imports/Part _A/utils.js
@@ -0,0 +1,7 @@
+const addition = (a, b) => a + b;
+
+const subtraction = (a, b) => a - b;
+
+const multiplier = (a, b) => a * b;
+
+module.exports = { addition, subtraction, multiplier };
