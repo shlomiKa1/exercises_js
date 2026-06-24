3d5e8eb30a1d723aaf7572e2ed395214a5a66b75
Shlomi <shlomikaufman91@gmail.com>
2026-06-24 14:58:33 +0300

    Add exercise 5 of modules

---
+++ week_01/imports/part_B/array.js
@@ -0,0 +1,5 @@
+export const count = (arr) => arr.length;
+
+export const sumNums = (arr) => arr.reduce((acc, current) => acc + current, 0);
+
+export const union = (str) => str.join();
--- week_01/imports/part_B/main.js
+++ week_01/imports/part_B/main.js
@@ -1,6 +1,7 @@
 import { sum, PI } from "./math.js";
 import { greet } from "./named.js";
 import mult from "./math.js";
+import { count } from "./array.js";
 
 console.log("=== Exercise 1 ===");
 console.log(`1 + 3 = ${sum(1, 3)}`);
@@ -13,3 +14,6 @@ console.log(`PI = ${PI()}`);
 
 console.log("\n=== Exercise 4 ===");
 console.log(`1 * 3 = ${mult(1, 3)}`);
+
+console.log("\n=== Exercise 5 ===");
+console.log(`count of values in array: ${count([1, 2, 3, 4, 5])}`);
