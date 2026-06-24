a38bb740e6c7925652c651ab5644435d782e4578
Shlomi <shlomikaufman91@gmail.com>
2026-06-24 14:45:10 +0300

    Add exercise 3 of modules

--- week_01/imports/part_B/main.js
+++ week_01/imports/part_B/main.js
@@ -1,4 +1,4 @@
-import { sum } from "./math.js";
+import { sum, PI } from "./math.js";
 import { greet } from "./named.js";
 
 console.log("=== Exercise 1 ===");
@@ -6,3 +6,6 @@ console.log(`1 + 3 = ${sum(1, 3)}`);
 
 console.log("\n=== Exercise 2 ===");
 console.log(greet("Shlomi"));
+
+console.log("=== Exercise 3 ===");
+console.log(`PI = ${PI()}`);
--- week_01/imports/part_B/math.js
+++ week_01/imports/part_B/math.js
@@ -1,1 +1,3 @@
 export const sum = (a, b) => a + b;
+
+export const PI = Math.PI;
