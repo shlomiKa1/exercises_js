23c2e4a063299c6e65cf4d85564ef3656aacbfa6
Shlomi <shlomikaufman91@gmail.com>
2026-06-24 14:43:35 +0300

    Add exercise 1 of modules

--- week_01/imports/part_B/main.js
+++ week_01/imports/part_B/main.js
@@ -1,4 +1,8 @@
 import { sum } from "./math.js";
+import { greet } from "./named.js";
 
 console.log("=== Exercise 1 ===");
 console.log(`1 + 3 = ${sum(1, 3)}`);
+
+console.log("\n=== Exercise 2 ===");
+console.log(greet("Shlomi"));
---
+++ week_01/imports/part_B/named.js
@@ -0,0 +1,1 @@
+export const greet = (str) => `Hello ${str}`;
