7f31e2125a1335500894f36b1e761c883c59cd82
Shlomi <shlomikaufman91@gmail.com>
2026-06-24 14:51:24 +0300

    Add exercise 4 of modules

--- week_01/imports/part_B/main.js
+++ week_01/imports/part_B/main.js
@@ -1,5 +1,6 @@
 import { sum, PI } from "./math.js";
 import { greet } from "./named.js";
+import mult from "./math.js";
 
 console.log("=== Exercise 1 ===");
 console.log(`1 + 3 = ${sum(1, 3)}`);
@@ -7,5 +8,8 @@ console.log(`1 + 3 = ${sum(1, 3)}`);
 console.log("\n=== Exercise 2 ===");
 console.log(greet("Shlomi"));
 
-console.log("=== Exercise 3 ===");
+console.log("\n=== Exercise 3 ===");
 console.log(`PI = ${PI()}`);
+
+console.log("\n=== Exercise 4 ===");
+console.log(`1 * 3 = ${mult(1, 3)}`);
--- week_01/imports/part_B/math.js
+++ week_01/imports/part_B/math.js
@@ -1,3 +1,7 @@
 export const sum = (a, b) => a + b;
 
-export const PI = Math.PI;
+export const PI = () => Math.PI;
+
+export default function multiplier(a, b) {
+  return a * b;
+}
