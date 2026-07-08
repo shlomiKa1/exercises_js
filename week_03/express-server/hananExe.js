0c338d83f32b10b13b7cf78d5d75c5e9391b8d2a
Shlomi <shlomikaufman91@gmail.com>
2026-07-08 11:30:57 +0300

    feat: Add endpoint for query path

--- week_03/express-server/hananExe.js
+++ week_03/express-server/hananExe.js
@@ -6,4 +6,12 @@ server.get("/api", (req, res) => {
   res.send({ message: "Hello from express" });
 });
 
+server.get("/api/search", (req, res) => {
+  const { name } = req.query;
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+  res.json({ message: "Hello " + name });
+});
+
 server.listen(3000, () => console.log("Listening on port 3000"));
